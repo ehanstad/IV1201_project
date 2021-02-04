b176c5fe9a006f06b01be85740f339d5707567f7
Lucas Villarroel <vilucas7@gmail.com>
2021-02-04 14:33:01 +0100

    add heroku postbuild

--- app.js
+++ app.js
@@ -9,18 +9,17 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('frontend/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
 }
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
+});
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // error handler
