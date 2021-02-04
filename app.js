0d7937152ecd124bafda4c32ceef81f5db2a6f68
Lucas Villarroel <vilucas7@gmail.com>
2021-02-04 14:24:28 +0100

    send static build files if production

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
