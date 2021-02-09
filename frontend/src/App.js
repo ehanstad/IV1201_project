f798caf2feb22e638179c191812529575e51baf7
ehanstad <ehanstad@kth.se>
2021-02-09 15:34:25 +0100

    init create form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,84 @@
-import logo from './logo.svg';
+/**
+ * @file The component for adding a new applicant
+ * @author Erik Hanstad
+ */
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: '',
+      lname: '',
+      ssn: '',
+      email: '',
+      pass: '',
+    };
+  }
+
+  addApplicant = (e) => {
+    const {
+      fname,
+      lname,
+      ssn,
+      email,
+      pass,
+    } = this.state;
+    console.log(this.state);
+    console.log(fname);
+    console.log(lname);
+    console.log(ssn);
+    console.log(email);
+    console.log(pass);
+    console.log(e);
+  };
+
+  fnameChange = (e) => {
+    this.setState({ fname: e.target.value });
+  };
+
+  lnameChange = (e) => {
+    this.setState({ lname: e.target.value });
+  };
+
+  ssnChange = (e) => {
+    this.setState({ ssn: e.target.value });
+  };
+
+  emailChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  passChange = (e) => {
+    this.setState({ pass: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Add applicant</h2>
+        <form onSubmit={this.addApplicant}>
+          <div>
+            <p>First name:</p>
+            <input type="text" id="fname" onChange={this.fnameChange} />
+            <p>Last name:</p>
+            <input type="text" id="lname" onChange={this.lnameChange} />
+            <p>Social security number:</p>
+            <input type="text" id="ssn" onChange={this.ssnChange} />
+            <p>Email:</p>
+            <input type="text" id="email" onChange={this.emailChange} />
+            <p>Password:</p>
+            <input type="password" id="password" onChange={this.passChange} />
+          </div>
+          <button type="submit" data-testid="sendButton">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
