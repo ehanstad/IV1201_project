f8905aaa1229d5832ccc77e366f3396de24ca8d5
Lucas Villarroel <vilucas7@gmail.com>
2021-02-15 14:52:23 +0100

    split directories

--- backend/routes/api/registration.js
+++ backend/routes/api/registration.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post('/', (req, res) => {
   newUser(req.body.fname, req.body.lname, req.body.ssn, req.body.email)
-  .then((dbRes) => {
+    .then((dbRes) => {
       res.json(dbRes);
     }).catch((dbErr) => {
       res.status(500).json(dbErr);
