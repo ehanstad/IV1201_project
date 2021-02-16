d7d8f9ba6caf99773b74e0ae9023f70bd0562817
ehanstad <ehanstad@kth.se>
2021-02-16 11:35:04 +0100

    implement router and registration

---
+++ frontend/src/components/login.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @requires
+ * @author Erik Hanstad
+ */
+
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        LOGIN
+        <a href="./registration">reg</a>
+      </div>
+    );
+  }
+}
+
+export default Login;
