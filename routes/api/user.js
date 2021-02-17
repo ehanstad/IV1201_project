ceab72be1f30a034a7c7ebb3ecb9ecc97c46856a
Lucas Villarroel <vilucas7@gmail.com>
2021-02-17 10:49:00 +0100

    rename endpoint

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,13 +4,13 @@ const { newUser } = require('../../db');
 const router = Router();
 
 /**
- * 
+ *
   Sends registration form data to db module.
   Responds with either a success or error 500.
  */
 router.post('/', (req, res) => {
   newUser(req.body.fname, req.body.lname, req.body.ssn, req.body.email)
-  .then((dbRes) => {
+    .then((dbRes) => {
       res.json(dbRes);
     }).catch((dbErr) => {
       res.status(500).json(dbErr);
