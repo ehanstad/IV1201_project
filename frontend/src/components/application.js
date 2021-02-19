f89b91fab4058e97fd418e77e22f7824b885df8b
Lucas Villarroel <vilucas7@gmail.com>
2021-02-19 17:57:13 +0100

    lint fixes

--- frontend/src/components/application.js
+++ frontend/src/components/application.js
@@ -6,11 +6,7 @@
 import React from 'react';
 
 function Application() {
-  return (
-    <div>
-      Application
-    </div>
-  );
+  return <div>Application</div>;
 }
 
 export default Application;
